c01e2fa5f7b7579b0d87e3997aa26560d1dd91d8
yaduvamsikrishna <164786622+yaduvamsikrishna2004@users.noreply.github.com>
2025-12-04 21:05:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Cast the entire devIndicators object to 'any' or 'object'
   devIndicators: {
     buildActivity: false,
     // @ts-ignore
     appIsrStatus: false,
-  },
+  } as any, // 👈 ADD 'as any' here
 };
 
 export default nextConfig;
